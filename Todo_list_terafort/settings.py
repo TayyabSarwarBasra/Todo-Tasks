dde493cdbf62958d9ce98971d666ccff81a0708c
Tayyab Basra <91774969+TayyabSarwarBasra@users.noreply.github.com>
2024-04-19 09:59:16 +0500

    Update settings.py
    remove the password

--- Todo_list_terafort/settings.py
+++ Todo_list_terafort/settings.py
@@ -120,7 +120,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'tayyabbasra434@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'cdawswetmlxeqhya')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD',)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'tayyabbasra434@gmail.com')
 
